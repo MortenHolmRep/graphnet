be7b0511624675030e4e04e3c8a943aca86ee290
Morten Holm <qgf305@hep02.cluster>
2022-10-04 23:58:27 +0200

    code refactoring

--- studies/Moon_Pointing_Analysis/plotting/event_location/event_heatmap.py
+++ studies/Moon_Pointing_Analysis/plotting/event_location/event_heatmap.py
@@ -2,8 +2,10 @@ import sqlite3 as sql
 from plot_params import *
 from pandas import read_sql
 import numpy as np
+import itertools
+from matplotlib.colors import LogNorm
 
-bins = 50
+bins = 25
 
 # data pathing
 indir = "/groups/icecube/peter/storage/MoonPointing/data/Sschindler_data_L4/Merged_database/Merged_database.db"
@@ -21,21 +23,32 @@ with sql.connect(indir) as con:
     """
     sql_data = read_sql(query,con)
 
-hist, xedge, yedge = np.histogram2d(sql_data["dom_x"], sql_data["dom_y"], bins=bins)
+#hist, xedge, yedge = np.histogram2d(sql_data["dom_x"], sql_data["dom_y"], bins=bins)
 
-plt.figure(figsize=single)
-X, Y = np.meshgrid(xedge, yedge)
-plt.pcolormesh(X, Y, hist, cmap = 'coolwarm')
-plt.colorbar(label="triggers")
-plt.savefig(outdir + "trigger_density_pmesh.png")
+#plt.figure(figsize=single)
+#X, Y = np.meshgrid(xedge, yedge, sparse=True)
+#plt.pcolormesh(X, Y, hist, cmap = 'coolwarm')
+#plt.colorbar(label="triggers")
+#plt.savefig(outdir + "trigger_density_pmesh.png")
 
-plt.figure(figsize=single)
-# Creating plot
-plt.hexbin(
-    sql_data["dom_x"], sql_data["dom_y"], gridsize=bins, cmap = 'coolwarm'
-    )
-plt.colorbar(label="triggers")
-plt.xlabel("x position")
-plt.ylabel("x position")
-plt.title(f"trigger density")
+# assign empty containers and plotting parameters
+grid = plt.GridSpec(1,3, wspace=0.6, hspace=.3)
+fig,ax = plt.subplots(1,3,figsize=triple)
+
+## plot into containers
+for i, dom in enumerate(itertools.combinations(["dom_x", "dom_y", "dom_z"], 2)): 
+    plt.subplot(grid[0, i])
+    # Creating plot
+    maxima = max(np.concatenate((sql_data[dom[0]],sql_data[dom[1]]),axis=None))
+    mappable = plt.hexbin(
+        sql_data[dom[0]], sql_data[dom[1]], gridsize=bins, 
+        norm=LogNorm(),
+        cmap = 'coolwarm'
+        )
+    colorbar(mappable)
+    plt.xlabel(dom[0]+" position")
+    plt.ylabel(dom[1]+" position")
+    plt.title(f"trigger density")
+
+
 plt.savefig(outdir + "trigger_density.png")
