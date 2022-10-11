feec453c5a5a7c8011dcc150c7070b8c586f288e
MortenHolmRep <Volumunox@gmail.com>
2022-10-11 09:13:51 -0500

    more readability

--- studies/Moon_Pointing_Analysis/data/data_pipeline.sh
+++ studies/Moon_Pointing_Analysis/data/data_pipeline.sh
@@ -1,7 +1,13 @@
-db=/mnt/lfs6/sim/IceCube/2012/filtered/level2/neutrino-generator/11069/08000-08999/
+db=/mnt/lfs6/sim/IceCube/2012/filtered/level2/neutrino-generator/11069/09000-09999/
 gcd=/mnt/lfs6/sim/IceCube/2012/filtered/level2/neutrino-generator/11069/08000-08999/GeoCalibDetectorStatus_2012.56063_V1.i3.gz
 pulse="InIceDSTPulses"
 
 outdir=/scratch/mholm/database/
+report_location=/data/user/mholm/data/nohup_reports/MCL2.out
 
-python /data/user/mholm/graphnet/studies/Moon_Pointing_Analysis/data/convert_i3_to_sqlite.py --db ${db} --gcd ${gcd} --pulse ${pulse} --outdir ${outdir}
+nohup python /data/user/mholm/graphnet/studies/Moon_Pointing_Analysis/data/convert_i3_to_sqlite.py \
+--db ${db} \
+--gcd ${gcd} \
+--pulse ${pulse} \
+--outdir ${outdir} \
+> ${report_location}
