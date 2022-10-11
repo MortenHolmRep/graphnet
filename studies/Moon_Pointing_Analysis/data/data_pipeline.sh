d209a4bba016de3521cdda269146242948ecf933
MortenHolmRep <Volumunox@gmail.com>
2022-10-11 08:46:25 -0500

    working cobalt data converter bash

--- studies/Moon_Pointing_Analysis/data/data_pipeline.sh
+++ studies/Moon_Pointing_Analysis/data/data_pipeline.sh
@@ -1,5 +1,7 @@
-db=/groups/icecube/qgf305/storage/I3_files/Sebastian_MoonL4/
-gcd=None
-pulse="SRTInIcePulses"
+db=/mnt/lfs6/sim/IceCube/2012/filtered/level2/neutrino-generator/11069/08000-08999/
+gcd=/mnt/lfs6/sim/IceCube/2012/filtered/level2/neutrino-generator/11069/08000-08999/GeoCalibDetectorStatus_2012.56063_V1.i3.gz
+pulse="InIceDSTPulses"
 
-python convert_i3_to_sqlite.py --db ${db} --gcd ${gcd} --pulse ${pulse}
+outdir=/scratch/mholm/database/
+
+python /data/user/mholm/graphnet/studies/Moon_Pointing_Analysis/data/convert_i3_to_sqlite.py --db ${db} --gcd ${gcd} --pulse ${pulse} --outdir ${outdir}
